50d98ef99b5f64605691a20f911dcabbe36c197a
Artem <artem@wearemad.ru>
2021-03-06 13:33:09 +0300

    + refactoring

--- app/src/main/java/ru/wearemad/navigation/MainActivity.kt
+++ app/src/main/java/ru/wearemad/navigation/MainActivity.kt
@@ -1,18 +1,11 @@
 package ru.wearemad.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.wearemad.mad_navigation.CiceroneHolder
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val h = CiceroneHolder()
-        val r = h.getOrCreateRouter("asd")
-        if (r != null) {
-
-        }
     }
 }
